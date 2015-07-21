b581d572bc2803d16521122ee79e00e7c6035245
Aidan Follestad <drummer.aidan@gmail.com>
2015-07-21 14:18:05 -0500

    Removed un-used sample project code

--- sample/src/main/java/com/afollestad/bridgesample/MainActivity.java
+++ sample/src/main/java/com/afollestad/bridgesample/MainActivity.java
@@ -4,15 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
 import com.afollestad.bridge.Bridge;
-import com.afollestad.bridge.Callback;
-import com.afollestad.bridge.Request;
-import com.afollestad.bridge.RequestException;
-import com.afollestad.bridge.Response;
-
-import org.json.JSONObject;
 
 /**
  * @author Aidan Follestad (afollestad)
@@ -27,23 +20,6 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView list = (RecyclerView) findViewById(R.id.list);
         list.setLayoutManager(new GridLayoutManager(this, 8));
         list.setAdapter(new MainAdapter());
-
-        Bridge.client().host("https://demoapi.salesfitness.net");
-        JSONObject body;
-        try {
-            body = new JSONObject("{\"password\":\"IamAidan18\",\"userName\":\"Young\",\"deviceToken\":\"DefaultDeviceToken\"}");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-        Bridge.client().post("/users/auth")
-                .body(body)
-                .request(new Callback() {
-                    @Override
-                    public void response(Request request, Response response, RequestException e) {
-                        Log.v("Done", "Done");
-                    }
-                });
     }
 
     @Override
